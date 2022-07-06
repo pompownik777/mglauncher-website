7268e6f80e6a49fe6ab66d58c2924940f5ee33b1
Mateusz <105235831+pompownik777@users.noreply.github.com>
2022-07-06 14:03:10 +0200

    final

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function Download() {
-    alert("Launcher do pobrania będzie możliwy od 22.05.2022");
+function pobierz() {
+    var element = document.createElement('a');
+    element.setAttribute('href', "MGLauncher 2.1.zip");
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
 }
